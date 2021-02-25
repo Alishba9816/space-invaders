d80482331f12c7ac241e53286b042fc829a2148d
A1i5h8a <alishbak1998@gmail.com>
2021-02-25 11:54:36 +0000

    i just made a quit program

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import random
 import pygame
-import pygame.locals import *
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode((600,500))
+pygame.display.set_caption("flappy birds")
+gameExit = False
+while not gameExit:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      gameExit = True
+
+pygame.quit()
+
+
