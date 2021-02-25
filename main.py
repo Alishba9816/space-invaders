db7e4c7d5029c4af88d7c2f008b4fadc745d250f
A1i5h8a <alishbak1998@gmail.com>
2021-02-25 12:20:33 +0000

    i added color to background

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import random
 import pygame
 from pygame.locals import *
 pygame.init()
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
 screen = pygame.display.set_mode((600,500))
 pygame.display.set_caption("flappy birds")
 gameExit = False
@@ -9,7 +12,6 @@ while not gameExit:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       gameExit = True
-
+  screen.fill(white)
+  pygame.display.update()
 pygame.quit()
-
-
