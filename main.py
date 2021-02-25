21c00ae0ed0c1cb64c1df9ac870b7dde8761cdf7
A1i5h8a <alishbak1998@gmail.com>
2021-02-25 18:26:11 +0000

    i added background image

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ pygame.init()
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
-screen = pygame.display.set_mode((600,500))
+GAME_SPRITES={}
+icon=pygame.image.load('lo.png')
+bgimg= pygame.image.load('giimg.jpg')
+screen = pygame.display.set_mode((450,400))
 pygame.display.set_caption("flappy birds")
+pygame.display.set_icon(icon)
 gameExit = False
 while not gameExit:
+  screen.fill(white)
+  screen.blit(bgimg, (0,0))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       gameExit = True
-  screen.fill(white)
   pygame.display.update()
 pygame.quit()
